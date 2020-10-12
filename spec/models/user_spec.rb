d7f5f6330148c969fe1170db7719ea7f1c96b692
aubry prieur <aubry.prieur@gmail.com>
2020-10-12 16:47:56 +0200

    Add user validations

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe User, type: :model do
+  describe "creation" do
+    before do
+      @user = User.create(email: "test@test.com", first_name: "aubry", last_name: "prieur", password: "azerty", password_confirmation: "azerty")
+    end
+
+    it "Can be created" do
+      expect(@user).to be_valid
+    end
+
+    it "cannot be created without first_name, last_name" do
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+
+  end
+end
