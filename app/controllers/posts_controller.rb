903458d7a91028b166f3e9a4c05c974c6181dcfe
aubry prieur <aubry.prieur@gmail.com>
2020-10-13 12:33:47 +0200

    Implemented creation process for posts and refactored

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,35 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "L'appel d'offres vient d'être créé!"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:deadline, :title, :description)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+
+
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -10,6 +10,25 @@ describe 'navigate' do
       visit posts_path
       expect(page).to have_content(/Liste des appels d'offres/)
     end
+  end
+  describe 'creation' do
+    before do
+      visit new_post_path
+    end
+
+    it 'has a new form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
+    it 'can be created from new form page' do
+      fill_in 'post[deadline]', with: Date.today
+      fill_in 'post[title]', with: "Un titre"
+      fill_in 'post[description]', with: "Une description"
+
+      click_on "Save"
+
+      expect(page).to have_content("Un titre")
+    end
 
   end
 end
+
