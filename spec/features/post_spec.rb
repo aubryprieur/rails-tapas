0ace06d4c2ad39b24b80753a8380d6899f65abcb
aubry prieur <aubry.prieur@gmail.com>
2020-10-15 16:57:20 +0200

    Implemented a list of posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,21 +2,31 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    user = User.create(email: "test@test.com", password: "azerty", password_confirmation: "azerty", first_name: "Jon", last_name: "Snow")
-    login_as(user, :scope => :user)
+    @user = User.create(email: "test@test.com", password: "azerty", password_confirmation: "azerty", first_name: "Jon", last_name: "Snow")
+    login_as(@user, :scope => :user)
   end
 
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
       visit posts_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Liste des appels d'offres/)
     end
+
+    it 'has a list of posts' do
+      post1 = Post.create(deadline: Date.today, title: "Appel d'offres 1", description: "Description1", user_id: @user.id)
+      post2 = Post.create(deadline: Date.today, title: "Appel d'offres 2", description: "Description 2", user_id: @user.id)
+      visit posts_path
+      expect(page).to have_content(/Description1|Description 2|Appel d'offres 1|Appel d'offres 2/)
+    end
   end
+
   describe 'creation' do
     before do
       visit new_post_path
