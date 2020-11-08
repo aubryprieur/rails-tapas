67f8f5d13c997a39f2a27baac389694e33cdb1a0
aubry prieur <aubry.prieur@gmail.com>
2020-11-08 19:16:49 +0100

    implemented ability to edit Posts first step

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit]
 
   def index
     @posts = Post.all
@@ -21,6 +21,9 @@ class PostsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
   end
 
   private
