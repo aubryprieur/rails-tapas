9454422924ceccb5d18c77ac67fae736b05ce792
aubry prieur <aubry.prieur@gmail.com>
2020-11-14 18:08:13 +0100

    changed the form for editing posts and partial card post

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
       resources :users
       resources :admin_users
       resources :posts
-
       root to: "users#index"
     end
+
   resources :posts
   devise_for :users, skip: [:registrations]
   root to: 'static#homepage'
